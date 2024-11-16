cd75bc0c5955bb8e8aa953a1c79f30b62394c98a
4bderahim <abderahimelkridah@gmail.com>
2024-11-16 17:44:08 +0100

    parsed

--- map.c
+++ map.c
@@ -22,9 +22,7 @@ char *next_line(int fd, int len)
         if (rt == -1)
             exit(1);
         if (rt == 0 && ft_strlen(next_line) == 0)
-        {
             return (NULL);
-        }
         if (rt == 0 || (len == 1 && *line == '\n'))
         {
             free(line);
@@ -38,6 +36,7 @@ char *next_line(int fd, int len)
     }
     return (next_line);
 }
+
 char *line(char *s)
 {
     int i;
@@ -84,6 +83,56 @@ int get_index(char *s)
     return (i);
 }
 
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	sign;
+
+	number = 0;
+	sign = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*(str + 1) == '+' || *(str + 1) == '-')
+			return (0);
+		else if (*str == '-')
+			sign = 1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		number *= 10;
+		number += (*str - 48);
+		str++;
+	}
+	if (sign == 1)
+		return (-number);
+	return (number);
+}
+int cf_color_not_valid(char *str)
+{
+    int i;
+    i = 0;
+
+    while (str[i] && str[i] != '\n')
+    {
+        if (str[i] < '0' || str[i] > '9')
+        {
+            if (!(str[i] >= 9 && str[i] <= 13) && str[i] != 32 && str[i] != '-' && str[i] != ',')
+                return (1);
+        }
+        if (str[i] >= '0' && str[i] <= '9')
+        {
+            if (str[i+1] && str[i+1] == ' ')
+                return (1);
+
+        }
+        i++;
+    }
+    return (0);
+}
+
 int set_fr_fg_fb(t_cu *cu, char *s, int x)
 {
     int i;
@@ -92,21 +141,21 @@ int set_fr_fg_fb(t_cu *cu, char *s, int x)
     i = get_index(s);
     s[i] = '\0';
     if (x)
-        cu->fr = atoi(s);
+        cu->fr = ft_atoi(s);
     else
-        cu->cr = atoi(s);
+        cu->cr = ft_atoi(s);
     s = s + i + 1;
     i = get_index(s);
     s[i] = 0;
     if (x)
-        cu->fg = atoi(s);
+        cu->fg = ft_atoi(s);
     else
-        cu->cg = atoi(s);
+        cu->cg = ft_atoi(s);
     s = s + i + 1;
     if (x)
-        cu->fb = atoi(s);
+        cu->fb = ft_atoi(s);
     else
-        cu->cb = atoi(s);
+        cu->cb = ft_atoi(s);
     return (1);
 }
 
@@ -115,19 +164,19 @@ int set_fc__(t_parsed_data *data_set, char *str , t_cu *cu, char c)
     if (str[data_set->i + 1] != ' ' || str[data_set->i + 2] == 0 || !len_(str + 2))
         return (0);
     if (c == 'F')
-        {
-            if (data_set->f == 1)
-                return (0);
-            set_fr_fg_fb(cu, str + data_set->i + 2, 0);
-            data_set->f = 1;
+    {
+        if (data_set->f == 1 || cf_color_not_valid(str + data_set->i + 2))
+            return (0);
+        set_fr_fg_fb(cu, str + data_set->i + 2, 0);
+        data_set->f = 1;
         }
     else
-        {
-            if ( data_set->c == 1)
-                return (0);
-            set_fr_fg_fb(cu, str + data_set->i + 2, 1);
-            data_set->c = 1;
-        }
+    {
+        if ( data_set->c == 1 || cf_color_not_valid(str + data_set->i + 2))
+            return (0);
+        set_fr_fg_fb(cu, str + data_set->i + 2, 1);
+        data_set->c = 1;
+    }
     return (1);
 }
 
@@ -461,7 +510,6 @@ int check_map(char **map)
     }
     return (1);
 }
-
 
 int not_valid(int i, int j, int map_len , char **map)
 {
